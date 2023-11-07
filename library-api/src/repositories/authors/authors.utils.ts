f4bb89fbdd12da1fa742952907d116d66368a443
MaesMM <monteil.m.t@gmail.com>
2023-11-07 20:34:14 +0100

    fonctionnal : Setup CRUD Book

---
+++ library-api/src/repositories/authors/authors.utils.ts
@@ -0,0 +1,19 @@
+import { Author } from 'library-api/src/entities';
+// import {
+//   AuthorRepositoryOutput,
+//   AuthorRepositoryOutput,
+// } from 'library-api/src/repositories/Authors/Author.repository.type';
+
+// export const adaptBookEntityToPainAuthorkModel = (
+//   author: Author,
+// ): AuthorRepositoryOutput => ({
+//   ...author,
+//     books: author.books.map((book) => book.book),
+// });
+
+// export const adaptAuthorEntityToAuthorModel = (
+//   author: Author,
+// ): AuthorRepositoryOutput => ({
+//     ...author,
+//     books: author.books.map((book) => book.book.title),
+// });

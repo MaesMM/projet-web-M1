4ee6725aa84c743f401bde16c3160c9bf9253c87
MaesMM <monteil.m.t@gmail.com>
2023-11-08 20:53:24 +0100

    feature : Create book & dataset

--- library-api/src/controllers/books/book.controller.ts
+++ library-api/src/controllers/books/book.controller.ts
@@ -5,17 +5,17 @@ import {
   PlainBookPresenter,
 } from 'library-api/src/controllers/books/book.presenter';
 import { BookId } from 'library-api/src/entities';
-import { BookUseCases } from 'library-api/src/useCases';
+import { AuthorUseCases, BookUseCases } from 'library-api/src/useCases';
 import { CreateBookDto } from './create-book.dto';
 import { CreateBookRepositoryInput } from 'library-api/src/repositories/books/book.repository.type';
-import { CreateBookUseCasesInput } from 'library-api/src/useCases/books/book.useCases.type';
-
-
+import { NotFoundError } from 'rxjs';
 @ApiTags("Books")
 
 @Controller('books')
 export class BookController {
-  constructor(private readonly bookUseCases: BookUseCases) {}
+
+  constructor(private readonly bookUseCases: BookUseCases, private readonly authorUseCases : AuthorUseCases) {}
+
 
   @Get('/')
   public async getAll(): Promise<PlainBookPresenter[]> {
@@ -33,22 +33,28 @@ export class BookController {
 
  
   //BEGIN REQUEST CREATE
-  // @Post('/create')
-  // public async createBook(@Body() bodyContent : CreateBookDto ) : Promise<void>{   // ou Promise<CreateBookUseCasesInput> ??
-  //   //get author id 
-  //   en attente des methods read author
-  //   if(bodyContent.authorId){
-  //     const author = await this.authorUseCases.getAuthorById(bodyContent.authorId);
-  //     if(author){
-  //       bodyContent.author = author;
-  //     }
-  //   }
-  //   const book= await this.bookUseCases.create(bodyContent);
-  //   // return BookPresenter.from(book);   
-  // }
-  // END REQUEST CREATE
+  @Post('/create')
+  public async createBook(@Body() bodyContent : CreateBookDto ) : Promise<BookPresenter>{   // ou Promise<BookPresenter> pour renvoyer un objet
+    const author = await this.authorUseCases.getById(bodyContent.authorId);
+    
+    if (!author) {
+      // Handle the case where the author with the given ID doesn't exist
+      throw new NotFoundError(`Author - '${bodyContent.authorId}'`);
+    }
 
-  // @Patch('/:id')
+    const newbook: CreateBookRepositoryInput = {
+      name: bodyContent.name,
+      writtenOn: bodyContent.writtenOn,
+      author: author,
+      genres: bodyContent.genres,
+    };
+
+    const createdBook = await this.bookUseCases.create(newbook);
+    console.log(createdBook);
+    return BookPresenter.from(createdBook);
+  }
+
+  // @Patch('/:id')  
   // public async updateBook(
   //   @Param('id') id: BookId,
   //   @Body() bodyContent: CreateBookDto,

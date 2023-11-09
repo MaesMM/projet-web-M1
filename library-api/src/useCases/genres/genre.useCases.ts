ae4bea982a4ed34e53849ee017bd5fb6b8929cdb
Mael Monteil <Monteil.m.t@gmail.com>
2023-11-09 22:36:03 +0100

    feat : genre Read by id end-point

--- library-api/src/useCases/genres/genre.useCases.ts
+++ library-api/src/useCases/genres/genre.useCases.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { GenreRepository } from 'library-api/src/repositories';
+import { GenreUseCasesOutput } from './genre.useCases.type';
+import { GenreModel } from 'library-api/src/models';
+import { GenreId } from 'library-api/src/entities';
 
 @Injectable()
-export class GenreUseCases {}
+export class GenreUseCases {
+  constructor(private readonly bookRepository: GenreRepository) {}
+
+  /**
+   * Get all plain books
+   * @returns Array of plain books
+   */
+  public async getAllPlain(): Promise<GenreUseCasesOutput[]> {
+    return this.bookRepository.getAllPlain();
+  }
+
+
+    /**
+     * Get a book by its ID
+     * @param id Book's ID
+     * @returns Book if found
+     * @throws 404: book with this ID was not found
+     */
+    public async getById(id: GenreId): Promise<GenreUseCasesOutput> {
+      return this.bookRepository.getById(id);
+    }
+}

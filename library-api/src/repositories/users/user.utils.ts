df0ec6b45838fdfdc6ec814ee8ec914a0f74d5b6
Schivass <73424747+Schivass@users.noreply.github.com>
2023-11-09 12:02:23 +0100

    User Implements & Genres Get

---
+++ library-api/src/repositories/users/user.utils.ts
@@ -0,0 +1,20 @@
+import { User } from 'library-api/src/entities';
+import {
+  UserRepositoryOutput,
+  PlainUserRepositoryOutput,
+} from 'library-api/src/repositories/users/user.repository.type';
+
+export const adaptUserEntityToPlainUserModel = (
+  user: User,
+): PlainUserRepositoryOutput => ({
+  ...user,
+  favoriteBook: user.userBook.map((userBook) => userBook.book.name),
+  userBook: user.userBook.map((userBook) => userBook.book.name),
+});
+
+export const adaptUserEntityToUserModel = (
+  user: User,
+): UserRepositoryOutput => ({
+  ...user,
+  userBook: user.userBook.map((userBook) => userBook.book),
+});
---
+++ library-api/src/useCases/users/user.useCases.type.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { UserId } from 'library-api/src/entities';
+import { UserRepository } from 'library-api/src/repositories/users/user.repository';
+import { PlainUserUseCasesOutput, UserUseCasesOutput } from './user.userCases';
+
+@Injectable()
+export class UserUseCases {
+  constructor(private readonly userRepostitory: UserRepository) {}
+
+  public async getAllplain(): Promise<PlainUserUseCasesOutput[]> {
+    return this.userRepostitory.getAllPlain();
+  }
+
+  public async getById(id: UserId): Promise<UserUseCasesOutput> {
+    return this.userRepostitory.getById(id);
+  }
+}

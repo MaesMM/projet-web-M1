2ebef4332561940cf89fcb4538591117923506f0
Antoine Maes <92756820+AntoLeRigoloo@users.noreply.github.com>
2023-11-10 14:12:18 +0100

    home page

--- library-site/src/app/page.tsx
+++ library-site/src/app/page.tsx
@@ -1,7 +1,38 @@
 'use client';
 
 import React, { ReactElement } from 'react';
+import Container from '@/component/container';
 
 export default function Home(): ReactElement {
-  return <>Hello</>;
+  return (
+    <div className="flex flex-col gap-8">
+      <Container className="flex flex-col gap-4" title="Bienvenue !">
+        <div className="flex flex-col gap-2">
+          <span className="font-medium">
+            Voici notre projet de gestionnaire de livre
+          </span>
+          <span className="italic">
+            Vous pouvez naviguer dans le menu de gauche pour accéder aux
+            différentes pages.
+          </span>
+        </div>
+      </Container>
+      <Container className="flex flex-col gap-4" title="A propos de nous">
+        <div className="flex flex-col gap-2">
+          <span className="font-medium">Back end</span>
+          <div className="flex flex-col text-sm italic">
+            <span>Nicolas BROAGE</span>
+            <span>Mael MONTEIL</span>
+            <span>Thomas VIGNERON</span>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <span className="font-medium">Front end</span>
+          <div className="flex flex-col text-sm italic">
+            <span>Antoine MAES</span>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }

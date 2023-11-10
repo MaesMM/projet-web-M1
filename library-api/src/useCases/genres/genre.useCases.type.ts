2a974ef064bef97273bc21bfa545acb938f68fa1
Nicolo41 <72193849+Nicolo41@users.noreply.github.com>
2023-11-10 15:58:30 +0100

    test

--- library-api/src/useCases/genres/genre.useCases.type.ts
+++ library-api/src/useCases/genres/genre.useCases.type.ts
@@ -2,6 +2,3 @@ import { GenreModel } from 'library-api/src/models';
 
 export type GenreUseCasesOutput = GenreModel;
 
-import { GenreModel } from 'library-api/src/models';
-
-export type GenreUseCasesOutput = GenreModel;
